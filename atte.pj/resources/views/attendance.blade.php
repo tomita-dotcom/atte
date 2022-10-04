8f6b3c88e6db21db7320e4b026cf9571cf96f0e6
tomita-dotcom <springroll1816@gmail.com>
2022-10-04 16:57:02 +0900

    10-4-1

--- atte.pj/resources/views/attendance.blade.php
+++ atte.pj/resources/views/attendance.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="css/reset.css" />
 <link rel="stylesheet" href="css/common.css" />
 <link rel="stylesheet" href="css/attendance.css" />
-<link href=“https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css” rel=“stylesheet” integrity=“sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3” crossorigin=“anonymous”>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 
 
@@ -51,7 +51,7 @@
       </tr>
       @endforeach
     </table>
-    {{ $works->appends($display_date)->links('vendor.pagination.bootstrap-4') }}
+    {{ $works->appends($display_date)->links() }}
   </div>
 
 @endsection
